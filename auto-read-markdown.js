9c0b8320e0a69724c38bbe33213c49c52c4fddb9
springleo <782655835@qq.com>
2019-01-18 18:02:01 +0800

    deploy at Fri Jan 18 18:02:01 CST 2019

--- auto-read-markdown.js
+++ auto-read-markdown.js
@@ -3,17 +3,24 @@ const path = require('path')
 
 let readMarkdown = (mapArray, { destpath, blogUrl }) => {
     let resultMd = []
+    let increse = 0
     mapArray.forEach(async (folderItem, fIndex) => {
         let promises = folderItem.children.map(path => fs.readFile((destpath + '/' + path), 'utf-8'))
         let contents = await Promise.all(promises)
-        contents = contents.map(md => /#(\s+)(\S*)\n/g.exec(md)[2])
+        contents = contents.map(md => {
+            let regexArr = /#(\s+)([\s\S]*?)\n/g.exec(md)
+            return (regexArr && regexArr[2]) || ''
+        })
 
         resultMd.push(`# ${folderItem.title}`)
         folderItem.children.forEach((path, index) => {
-            resultMd.push(`* [${contents[index]}](${blogUrl}/${path})`)
+            let title = `${contents[index] || path}`
+            let url = encodeURI(`${blogUrl}/${path.replace('.md', '.html')}`)
+            resultMd.push(`* [${title}](${url})`)
         })
 
-        if (mapArray.length - 1 === fIndex) {
+        increse++
+        if (mapArray.length - 1 === increse) {
             console.log(resultMd)
             let resultStr = resultMd.join('\n')
             await fs.outputFile('./README.md', resultStr)
